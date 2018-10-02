9375ea90523151f3f965774dcfbcbf1bb97cfcf2
unknown <fajarmaula7@gmail.com>
2018-10-02 19:16:12 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { KuisComponent } from './kuis/kuis.component';
+import { PegawaiComponent } from './pegawai/pegawai.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KuisComponent,
+    PegawaiComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/kuis/kuis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kuis',
+  templateUrl: './kuis.component.html',
+  styleUrls: ['./kuis.component.css']
+})
+export class KuisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
